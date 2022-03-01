a874eabb753cbb1bc41877265ee28005dc7684c3
Egor <abezriad@student.ubc.ca>
2022-03-01 13:32:38 -0800

    Добавил защиту от долбоёба в первом вводе

--- src/baza/fuck_you.java
+++ src/baza/fuck_you.java
@@ -1,5 +1,6 @@
 package baza;
 
+import java.io.BufferedReader;
 import java.util.Scanner;
 
 public class fuck_you {
@@ -8,31 +9,50 @@ public class fuck_you {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		int num;
+		int num = 0;
 		String s;										// добавить поддержку имён
 		
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		String s = br.readLine();
+//		int i = Integer.parseInt(s);
 		
-		
+		Scanner sc = new Scanner(System.in);
 		while(run) {
 			System.out.print("Сколько раз послать нахуй? ");
-			num = in.nextInt();							// впилить try-catch, легко наебать
+//			num = in.nextInt();							// впилить try-catch, легко наебать
+//			in.nextLine();
+			
+			
+		    boolean inError = true;
+		    
+		    while (inError) {
+		        if (sc.hasNextInt())
+		            num = sc.nextInt();
+		        else {
+		            sc.next();
+		            System.out.print("Сука цифру напиши а ");
+		            continue;
+		        	}
+		        inError = false;
+		    }
 			
 			if(checkValue(num))
 				printFuck(num);
 			else {
-				System.out.println("Всё хуйня, давай по новой.");
+				System.out.println("Всё хуйня, давай по новой. \n");
 				continue;
 			}
-			
+/*			
 			do {										// переделать, хуево
 				System.out.print("Послать ещё нахуй? ");
 				s = in.next();							// баг с проскакиванием ввода, надо придумать как починить
 			} while (!parseString(s));
-			
+*/			
 														// придумать как красиво сделать выход
 		}
+		sc.close();
 		
-		in.close();
+//		in.close();
 	}
 	
 	public static boolean parseString(String s) {
@@ -50,6 +70,7 @@ public class fuck_you {
 	public static void printFuck(int n) {
 		for(int i = 1; i <= n; i++)
 			System.out.println("Пошёл нахуй!");
+		System.out.println();
 	}
 	
 	public static boolean checkValue(int n) {
